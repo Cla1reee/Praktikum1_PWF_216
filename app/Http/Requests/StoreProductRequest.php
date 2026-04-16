a423a2773689b0fd2582cc7ce4611c4985e35d9e
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-04-16 09:12:33 +0700

    Update StoreProductRequest to authorize requests and define validation rules

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -6,23 +6,29 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreProductRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true; // WAJIB TRUE!
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name'  => 'required|string|max:255',
+            'qty'   => 'required|integer', 
+            'price' => 'required|numeric',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nama produk wajib diisi.',
+            'name.max'      => 'Nama produk tidak boleh lebih dari 255 karakter.',
+            'qty.required'  => 'Jumlah (kuantitas) produk wajib diisi.',
+            'qty.integer'   => 'Jumlah produk harus berupa angka bulat (tidak boleh desimal).',
+            'price.required'=> 'Harga produk wajib diisi.',
+            'price.numeric' => 'Harga produk harus berupa angka yang valid.',
         ];
     }
 }
